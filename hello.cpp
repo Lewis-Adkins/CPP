86d76d31167cfb8e386089deb663c8e12d0e7257
Lewis <130001684+Lewis-Adkins@users.noreply.github.com>
2024-01-27 18:35:01 -0500

    Delete hello.cpp

--- hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout << "Hello Bingus!"<<"Anthony\n";
-    cout << "Hello Dangus!"<<"Koda" << endl << endl;
-    cout << "Hello Bangus!"<<"Ollie";
-    return 0; 
-} 
